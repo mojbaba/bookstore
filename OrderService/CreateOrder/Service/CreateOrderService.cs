08ab2bbdc984eb2677f37d3e9c41c0d709624d3f
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-23 00:43:44 +0330

    rename models to RequestResponses

--- OrderService/CreateOrder/Service/CreateOrderService.cs
+++ OrderService/CreateOrder/Service/CreateOrderService.cs
@@ -1,6 +1,17 @@
+using OrderService.Entities;
+
 namespace OrderService.CreateOrder;
 
-public class CreateOrderService
+public class CreateOrderService(IOrderRepository repository) : ICreateOrderService
 {
-    
+    public Task<CreateOrderResponse> CreateOrderAsync(CreateOrderRequest request, CancellationToken cancellationToken)
+    {
+        var order = new OrderEntity
+        {
+            Status = OrderStatus.Processing
+        };
+
+        throw new NotImplementedException();
+    }
 }
+
