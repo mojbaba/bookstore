00fd9b8a5bcd737ce65df90197eb7cb2561d2749
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-22 19:28:32 +0330

    use proper way of IDatabase of redis

--- BookStore.Authentication.Jwt.Redis/RedisTokenValidationService.cs
+++ BookStore.Authentication.Jwt.Redis/RedisTokenValidationService.cs
@@ -2,26 +2,21 @@ using StackExchange.Redis;
 
 namespace BookStore.Authentication.Jwt.Redis;
 
-public class RedisTokenValidationService(string connectionString) : ITokenValidationService
+public class RedisTokenValidationService(IDatabase database) : ITokenValidationService
 {
-    private readonly ConnectionMultiplexer _redis = ConnectionMultiplexer.Connect(connectionString);
-
     public async Task<bool> ValidateTokenAsync(string token, CancellationToken cancellationToken)
     {
-        var db = _redis.GetDatabase();
-        var keyExists = await db.KeyExistsAsync(token);
+        var keyExists = await database.KeyExistsAsync(token);
         var valid = !keyExists;
         return valid;
     }
 
     public Task RevokeTokenAsync(string token, TimeSpan expiry, CancellationToken cancellationToken)
     {
-        var db = _redis.GetDatabase();
-        return db.StringSetAsync(token, "revoked", expiry);
+        return database.StringSetAsync(token, "revoked", expiry);
     }
 
     public void Dispose()
     {
-        _redis.Dispose();
     }
 }
--- BookStore.Authentication.Jwt.Redis/ServiceRegisteration.cs
+++ BookStore.Authentication.Jwt.Redis/ServiceRegisteration.cs
@@ -8,9 +8,6 @@ public static class ServiceRegisteration
     public static void AddRedisTokenValidationService(this IServiceCollection services,
         Func<IServiceProvider, string> connectionString)
     {
-        services.AddTransient<ITokenValidationService, RedisTokenValidationService>(c =>
-            new RedisTokenValidationService(connectionString(c)));
-        services.AddTransient<ITokenValidationServiceFactory, RedisTokenValidationServiceFactory>(c =>
-            new RedisTokenValidationServiceFactory(connectionString(c)));
+        services.AddTransient<ITokenValidationService, RedisTokenValidationService>();
     }
 }
