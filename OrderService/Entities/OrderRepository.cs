2017c8efb88ce5e55df0a6d274e4918c32414de6
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-22 23:11:54 +0330

    OrderRepository implemented

--- OrderService/Entities/OrderRepository.cs
+++ OrderService/Entities/OrderRepository.cs
@@ -1,39 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace OrderService.Entities;
 
-public class OrderRepository : IOrderRepository
+public class OrderRepository(OrderServiceDbContext dbContext) : IOrderRepository
 {
     public Task<OrderEntity?> GetAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return dbContext.Orders.FirstOrDefaultAsync(a => a.Id == id.ToString(), cancellationToken);
     }
 
     public Task<OrderEntity?> GetAsync(string id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return dbContext.Orders.FirstOrDefaultAsync(a => a.Id == id, cancellationToken);
     }
 
     public Task<OrderEntity> CreateAsync(OrderEntity entity, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(dbContext.Orders.Add(entity).Entity);
     }
 
     public Task<OrderEntity> UpdateAsync(OrderEntity entity, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(dbContext.Orders.Update(entity).Entity);
     }
 
     public Task<OrderEntity> DeleteAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var entity = dbContext.Orders.First(a => a.Id == id.ToString());
+        if(entity == null)
+        {
+            throw new InvalidOperationException("Entity not found");
+        }
+        
+        return Task.FromResult(dbContext.Orders.Remove(entity).Entity);
     }
 
     public Task<OrderEntity> DeleteAsync(string id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var entity = dbContext.Orders.First(a => a.Id == id);
+        if(entity == null)
+        {
+            throw new InvalidOperationException("Entity not found");
+        }
+        
+        return Task.FromResult(dbContext.Orders.Remove(entity).Entity);
     }
 
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return dbContext.SaveChangesAsync(cancellationToken);
     }
 }
