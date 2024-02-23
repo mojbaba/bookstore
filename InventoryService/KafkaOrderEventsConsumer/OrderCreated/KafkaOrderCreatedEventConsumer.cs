404ef69af97972b9de004ff5fabdb54a4a2697b3
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-23 23:38:50 +0330

    Inventoy left unfinished, my times up

---
+++ InventoryService/KafkaOrderEventsConsumer/OrderCreated/KafkaOrderCreatedEventConsumer.cs
@@ -0,0 +1,21 @@
+using System.Text.Json;
+using BookStore.Contracts;
+using BookStore.EventLog.Kafka;
+using BookStore.EventObserver;
+using Confluent.Kafka;
+using OrderService.CreateOrder;
+
+namespace InventoryService.KafkaOrderEventsConsumer;
+
+public class KafkaOrderCreatedEventConsumer(KafkaOptions kafkaOptions, IEventPublishObservant observant)
+    : KafkaConsumerBase<OrderCreatedEvent>(kafkaOptions, observant)
+{
+    protected override string GetTopic(KafkaOptions kafkaOptions) => kafkaOptions.Topics.OrderCreatedTopic;
+
+    protected override void HandleException(Exception e)
+    {
+        // must have a fail tolerance mechanism (e.g. retry, dead letter queue, etc.) no time to implement it, for now just log the exception and panic
+        Console.WriteLine(e);
+        throw e;
+    }
+}
