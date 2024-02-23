59114fd1e7fe8f749d344eb3b565710e19d83ea3
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-23 14:30:25 +0330

    add authentication header to request to inventory query

--- BookStore.Contracts/RequestResponses/QueryBookRequest.cs
+++ BookStore.Contracts/RequestResponses/QueryBookRequest.cs
@@ -3,4 +3,5 @@ namespace InventoryService.QueryBooks;
 public record QueryBookRequest
 {
     public IEnumerable<string> BookIds { get; set; } = new string[]{};
+    public string AuthenticationToken { get; set; }
 }
