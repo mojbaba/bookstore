26046b760200e49174752e2b730378bc7e66e7c5
Mojtaba Goodarzi <mojtaba73.g@gmail.com>
2024-02-23 14:37:02 +0330

    Create order controller added

--- OrderService/CreateOrder/Controller/CreateOrderController.cs
+++ OrderService/CreateOrder/Controller/CreateOrderController.cs
@@ -1,10 +1,45 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrderService.CreateOrder;
 
 [ApiController]
 [Route("api/order")]
-public class CreateOrderController : ControllerBase
+[Authorize]
+public class CreateOrderController(ICreateOrderService createOrderService) : ControllerBase
 {
-    
+ 
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateOrder(CreateOrderControllerRequest controllerRequest)
+    {
+        if(ModelState.IsValid == false)
+        {
+            return BadRequest();
+        }
+        
+        var jwtTokenInHeader = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+        var request = new CreateOrderRequest()
+        {
+            BookIds = controllerRequest.BookIds,
+            UserId = User.FindFirstValue(ClaimTypes.Sid),
+            AuthenticationToken = jwtTokenInHeader
+        };
+
+        try
+        {
+            var response = await createOrderService.CreateOrderAsync(request, CancellationToken.None);
+            return Ok(response);
+        }
+        catch (CreateOrderException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
+
+public record CreateOrderControllerRequest
+{
+    public IEnumerable<string> BookIds { get; set; }
 }
